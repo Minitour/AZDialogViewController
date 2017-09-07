54a28f7e64c50f8040ede1f986e80efb24372e12
Eric Marchand <eric.marchand@4d.com>
2017-09-07 17:06:49 +0200

    Add framework target in project and share it (carthage compatibility)

---
+++ AZDialogViewControllerExample/AZDialogViewController/AZDialogViewController.h
@@ -0,0 +1,19 @@
+//
+//  AZDialogViewController.h
+//  AZDialogViewController
+//
+//  Created by Eric Marchand on 07/09/2017.
+//  Copyright © 2017 Antonio Zaitoun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AZDialogViewController.
+FOUNDATION_EXPORT double AZDialogViewControllerVersionNumber;
+
+//! Project version string for AZDialogViewController.
+FOUNDATION_EXPORT const unsigned char AZDialogViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZDialogViewController/PublicHeader.h>
+
+
